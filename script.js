e9dfdf4c278ca5cd4b73486b3d8f3cd99a6f3770
Risma Dhiva Syabila <risma.dhivasyabilla@gmail.com>
2024-05-23 22:02:18 +0700

    add interactive dashboard

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+//humburger menu
+const menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.hamburger');
+const bars = document.querySelector('.icon-bars');
+const close = document.querySelector('.icon-close');
+
+hamburger.addEventListener('click',displayMenu);
+menu.addEventListener('click',displayMenu);
+
+//cross menu
+function displayMenu(){
+    if(menu.classList.contains('tampil')){
+        menu.classList.remove('tampil');
+        bars.style.display='inline';
+        close.style.display='none';
+    } else{
+        menu.classList.add('tampil');
+        bars.style.display='none';
+        close.style.display='inline';
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const filterForm = document.getElementById('filterForm');
     const netProfitElement = document.getElementById('netProfit');
